a7bea530499a38f8cb29f7b19fac0dcf5c8ccc76
koskei-kipkoech <patrickwayy@gmail.com>
2024-11-06 09:16:11 +0300

    updates src/components

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,36 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import AdminNavBar from "./AdminNavBar";
 import QuestionForm from "./QuestionForm";
 import QuestionList from "./QuestionList";
 
 function App() {
-  const [page, setPage] = useState("List");
+  const [questions, setQuestions] = useState([]);
+  const [page, setPage] = useState('List')
+  useEffect(() =>{
+    fetch('http://localhost:4000/questions')
+      .then((res) => res.json())
+      .then((data) => setQuestions(data))
+  },[])
+  function addQuestion(newQuestion){
+    setQuestions([...questions, newQuestion])
+  }
+  function deleteQuestion(id){
+    setQuestions(questions.filter((question) => question.id !== id));
+  }
+  function updateQuestion(updatedQuestion){
+    setQuestions(
+      questions.map((q) => (q.id === updatedQuestion.id ? updatedQuestion : q))
+    );
+  }
 
   return (
     <main>
       <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
+      {page === "Form" ? (
+        <QuestionForm onAddQuestion={addQuestion}/> 
+        ): (
+        <QuestionList questions={questions} onDeleteQuestion={deleteQuestion} onUpdateQuestion={updateQuestion}  />
+      )}
     </main>
   );
 }
--- src/components/QuestionForm.js
+++ src/components/QuestionForm.js
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
-function QuestionForm(props) {
+function QuestionForm({onAddQuestion}) {
   const [formData, setFormData] = useState({
     prompt: "",
     answer1: "",
@@ -9,6 +9,13 @@ function QuestionForm(props) {
     answer4: "",
     correctIndex: 0,
   });
+  let isMounted = true
+
+  useEffect(() => {
+    return () => {
+      isMounted = false;
+    }
+  },[])
 
   function handleChange(event) {
     setFormData({
@@ -19,8 +26,35 @@ function QuestionForm(props) {
 
   function handleSubmit(event) {
     event.preventDefault();
-    console.log(formData);
+    const newQuestion = {
+      prompt:formData.prompt,
+      answers: [formData.answer1,formData.answer2,formData.answer3,formData.answer4],
+      correctIndex:formData.correctIndex
+    }
+    addQuestion(newQuestion)
   }
+  function addQuestion(question){
+    fetch("http://localhost:4000/questions",{
+      method: "POST",
+      headers: {"Content-Type": "application/json"},
+      body:JSON.stringify(question), 
+    })
+      .then((res) => res.json())
+      .then((data) =>{
+        if(isMounted){
+          setFormData({
+            prompt: "",
+            answer1: "",
+            answer2: "",
+            answer3: "",
+            answer4: "",
+            correctIndex: 0
+          });
+          onAddQuestion(data)
+        }  
+      })
+      .catch((error)=> console.error("Error Adding Question:",error)) 
+    }
 
   return (
     <section>
